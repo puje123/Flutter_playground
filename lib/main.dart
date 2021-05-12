f9262c8a5faf39226ffbe7589e8069747f6c87d6
puje123 <nzireus@gmail.com>
2021-05-12 12:11:30 +0800

    homework

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'dart:math';
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'package:mongol/mongol.dart';
+
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FutureBuilder(
-        future: _fbApp,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text('Sorry, there was an error');
-          } else if (snapshot.hasData) {
-            return MyHomePage();
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(children: [
-          ElevatedButton(
-              child: Text('Update firebase database'),
-              onPressed: () {
-                print('hi');
-                DatabaseReference _testRef = FirebaseDatabase.instance.reference().child("test");
-                _testRef.set("Hello ${Random().nextInt(100)}");
-              }
-          )
-        ]),
-      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image(
+                image: AssetImage('assets/img/mori.jpg'),
+              ),
+              Container(
+                height: 400,
+                width: 400,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: MongolText(
+                    'ᠰᠠᠶᠢᠨ ᠪᠠᠶᠢᠨ᠎ᠠ ᠤᠤ? ᠮᠢᠨᠦ ᠨᠡᠷ᠎ᠡ ᠶᠢ ᠫᠦᠷᠪᠦᠳᠣᠷᠵᠢ ᠭᠡᠳᠡᠭ᠃ ᠣᠳᠣ 24 ᠨᠠᠰᠤᠲᠠᠢ᠃ ᠮᠠᠨ ᠤ ᠭᠡᠷ ᠪᠦᠯᠢ 4 ᠬᠦᠮᠦᠨ ᠪᠠᠶᠢᠳᠠᠭ᠃ ᠡᠵᠢ᠂ ᠠᠪᠤ᠂ ᠳᠡᠭᠦᠦ 3 ᠲᠠᠶᠢᠭ᠎ᠠ ᠪᠠᠨ ᠴᠤᠭ ᠠᠮᠢᠳᠤᠷᠠᠳᠠᠭ᠃ ᠪᠢ ᠡᠨᠡ ᠠᠫᠫ ᠢ ᠠᠰᠢᠭᠯᠠᠵᠤ ᠰᠠᠶᠢᠨ ᠰᠤᠷᠠᠭ᠎ᠠ ᠦᠭᠡᠢ ᠪᠠᠶᠢᠭ᠎ᠠ᠃ ᠡᠨᠡ ᠪᠣᠯ ᠮᠢᠨᠦ ᠭᠡᠷ ᠦᠨ ᠳᠠᠭᠠᠭᠠᠯᠭᠠᠪᠤᠷᠢ᠃',
+                    style:
+                        TextStyle(fontFamily: 'MongolianScript', fontSize: 25),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
     );
   }
 }
